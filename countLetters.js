53a1a8e47c780305e753116f733b239d651f6163
Rhys Wood <rhyswoody@gmail.com>
2021-10-28 01:15:27 +0000

    created a function which returns a count of letters in a given string

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+//This is a function which compares two inputs and evaluates whether they are the same
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  count = {};
+  for (const letters of sentence) {
+    if (letters != ' '){
+      if (count[letters]) {
+        count[letters] += 1;
+      } else {
+        count[letters] = 1;
+      }
+    }
+  }
+  return count;
+}
+
+const text = "Hello my name is rhys";
+// console.log(countLetters("Hello my name is rhys"));
+const result1 = countLetters(text);
+assertEqual(result1["H"], 1);
+// console.log(result1);
